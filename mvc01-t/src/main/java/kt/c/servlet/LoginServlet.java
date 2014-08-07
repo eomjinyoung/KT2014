3bfc3c13d2025cecd10de5cfd959a6ddf947ccc3
jinyoung <jinyoung.eom@gmail.com>
2014-08-07 11:56:08 +0900

    .

--- mvc01-t/src/main/java/kt/c/servlet/LoginServlet.java
+++ mvc01-t/src/main/java/kt/c/servlet/LoginServlet.java
@@ -16,8 +16,15 @@ public class LoginServlet extends HttpServlet {
 	@Override
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 	    throws ServletException, IOException {
+		System.out.println("LoginServlet.doGet()...");
 		RequestDispatcher rd = request.getRequestDispatcher("/view/auth/login.jsp");
 		rd.forward(request, response);
+	}
+	
+	@Override
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+	    throws ServletException, IOException {
+		System.out.println("LoginServlet.doPost()...");
 	}
 }
 
