8b6ea2804b49d2dcb2e502dc3cce2e6434966aed
jinyoung <jinyoung.eom@gmail.com>
2014-08-08 17:22:17 +0900

    .

--- mvc03-t/src/main/java/kt/c/util/ConnectionFactory.java
+++ mvc03-t/src/main/java/kt/c/util/ConnectionFactory.java
@@ -4,49 +4,57 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
+import java.util.ArrayList;
 
 public class ConnectionFactory {
-
-	public Connection getConnection() {
-		
-		Connection con = null;
-		
-		try {
+	ArrayList<Connection> conlist = new ArrayList<Connection>();
+	
+	public Connection getConnection() throws Exception {
+		if (conlist.size() > 0) {
+			return conlist.remove(0);
 			
+		} else {
 			Class.forName("oracle.jdbc.driver.OracleDriver");
 			
 			String url = "jdbc:oracle:thin:@192.168.0.16:1521:xe";
 			String user = "hr";
 			String password = "hr";
 			
-			con = DriverManager.getConnection(url, user, password);
-			
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		
-		return con;
+			return DriverManager.getConnection(url, user, password);
+		}	
 	}
 	
-	public void close(Connection con, PreparedStatement pstmt) {
-		
-		if(pstmt != null) {
-			try {
-				pstmt.close();
-			} catch (SQLException e) {
-				e.printStackTrace();
+	public void returnConnection(Connection con) {
+		try {
+			if (!con.isClosed() && con.isValid(1)) {
+				conlist.add(con);
 			}
-		}
-		
-		if(con != null) {
+		} catch (Exception e) {}
+	}
+	
+	public void closeAll() {
+		for (Connection con : conlist) {
 			try {
 				con.close();
-			} catch (SQLException e) {
-				e.printStackTrace();
-			}
+			} catch(Exception e) {}
 		}
 	}
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
