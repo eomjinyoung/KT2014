9b1ec16625f46361433387e441085a8a565bda77
jinyoung <jinyoung.eom@gmail.com>
2014-08-12 10:14:28 +0900

    .

---
+++ spring01/src/main/java/ex01/Car.java
@@ -0,0 +1,19 @@
+package ex01;
+
+public class Car {
+	String model;
+	int cc;
+	
+	public String getModel() {
+		return model;
+	}
+	public void setModel(String model) {
+		this.model = model;
+	}
+	public int getCc() {
+		return cc;
+	}
+	public void setCc(int cc) {
+		this.cc = cc;
+	}
+}
---
+++ spring01/src/main/java/ex01/Test02.java
@@ -0,0 +1,20 @@
+package ex01;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Test02 {
+
+	public static void main(String[] args) {
+		ApplicationContext 
+			ctx = new ClassPathXmlApplicationContext("ex01/beans.xml");
+
+		/*
+		 * getBean(아이디 또는 별명) : 컨테이너에서 해당 ID나 별명을 갖는 객체를 찾아서 리턴한다. 없으면 예외 발생!
+		 */
+		Car p = (Car)ctx.getBean("car1");
+		
+		Car p2 = (Car)ctx.getBean("car2");
+	}
+
+}
