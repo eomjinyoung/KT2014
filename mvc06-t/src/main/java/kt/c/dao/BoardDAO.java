4e4055a72ead05974ef8ed48baeb834fc5807db8
jinyoung <jinyoung.eom@gmail.com>
2014-08-13 19:41:24 +0900

    .

--- mvc06-t/src/main/java/kt/c/dao/BoardDAO.java
+++ mvc06-t/src/main/java/kt/c/dao/BoardDAO.java
@@ -1,335 +1,174 @@
 package kt.c.dao;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.util.ArrayList;
 import java.util.List;
-
-import javax.sql.DataSource;
 
 import kt.c.vo.BoardFileVO;
 import kt.c.vo.BoardVO;
 
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component("boardDAO")
 public class BoardDAO {
 	@Autowired
-	DataSource dataSource;
+	SqlSessionFactory sqlSessionFactory;
 
 	public int selectBoardNo() throws Exception {
-		StringBuilder sql = new StringBuilder();
-		sql.append("select seq_t_board_no.nextval from dual");
-		int boardNo = 0;
-
-		Connection con = null;
-		PreparedStatement pstmt = null;
-		ResultSet rs = null;
+		SqlSession sqlSession = sqlSessionFactory.openSession();
 
 		try {
-			con = dataSource.getConnection();
-			pstmt = con.prepareStatement(sql.toString());
-			rs = pstmt.executeQuery();
-
-			rs.next();
-			boardNo = rs.getInt(1);
-			return boardNo;
-
+			return sqlSession.selectOne("kt.c.dao.BoardDAO.selectBoardNo");
+			
 		} catch(Exception e) {
 			throw e;
 
 		} finally {
-			try {rs.close();} catch (Exception e) {}
-			try {pstmt.close();} catch (Exception e) {}
-			
-			// DataSource로부터 얻는 컨넥션 객체는 close()를 호출하면,
-			// DB서버와의 연결을 닫는 것이 아니라 DataSource에게 커넥션을 반납한다.
-			try {con.close();} catch (Exception e) {}
+			try {sqlSession.close();} catch (Exception e) {}
 		}
 	}
 
 	public List<BoardVO> selectAll() throws Exception {
-
-		List<BoardVO> list = new ArrayList<>();
-
-		StringBuilder sql = new StringBuilder();
-		sql.append("select no, title, writer, ");
-		sql.append("    to_char(reg_date, 'yyyy-mm-dd') reg_date ");
-		sql.append(" from t_board ");
-		sql.append(" order by no desc ");
-
-		Connection con = null;
-		PreparedStatement pstmt = null;
-		ResultSet rs = null;
-
+		// SqlSessionFactory 객체로부터 SqlSession 객체를 얻는다.
+		// => SqlSession 객체는 맵퍼 파일에 있는 SQL문을 실행한다. => 물론 내부에서 JDBC를 사용하여 처리한다.
+		SqlSession sqlSession = sqlSessionFactory.openSession();
+		
 		try {
-			con = dataSource.getConnection();
-			pstmt = con.prepareStatement(sql.toString());
-			rs = pstmt.executeQuery();
-
-			while(rs.next()) {
-				int no = rs.getInt("no");
-				String title = rs.getString("title");
-				String writer = rs.getString("writer");
-				String regDate = rs.getString("reg_date");
-
-				BoardVO board = new BoardVO();
-				board.setNo(no);
-				board.setTitle(title);
-				board.setWriter(writer);
-				board.setRegDate(regDate);
-
-				list.add(board);
-			}
-			return list;
+			//첫 번째 파라미터: 맵퍼 파일에 정의된 네임스페이스이름 + . + SQL문 아이디
+			//두 번째 파라미터: SQL문을 수행하는데 필요한 데이터를 객체에 담아서 넘긴다. 없으면 넘기지 않는다.
+			return sqlSession.selectList("kt.c.dao.BoardDAO.selectAll");
 			
 		} catch(Exception e) {
 			throw e;
 
 		} finally {
-			try {rs.close();} catch (Exception e) {}
-			try {pstmt.close();} catch (Exception e) {}
-			try {con.close();} catch (Exception e) {}
+			try {sqlSession.close();} catch (Exception e) {}
 		}
 	}
 
 	public BoardVO selectByNo(int no) throws Exception {
-
-		BoardVO board = new BoardVO();
-
-		StringBuilder sql = new StringBuilder();
-		sql.append("select no, title, writer, content, view_cnt, ");
-		sql.append("    to_char(reg_date, 'yyyy-mm-dd') reg_date ");
-		sql.append(" from t_board ");
-		sql.append(" where no = ? ");
-
-		Connection con = null;
-		PreparedStatement pstmt = null;
-		ResultSet rs = null;
+		SqlSession sqlSession = sqlSessionFactory.openSession();
 
 		try {
-			con = dataSource.getConnection();
-			pstmt = con.prepareStatement(sql.toString());
-			pstmt.setInt(1, no);
-			rs = pstmt.executeQuery();
-			rs.next();
-
-			board.setNo(no);
-			board.setTitle(rs.getString("title"));
-			board.setWriter(rs.getString("writer"));
-			board.setContent(rs.getString("content"));
-			board.setViewCnt(rs.getInt("view_cnt"));
-			board.setRegDate(rs.getString("reg_date"));
-			return board;
+			return sqlSession.selectOne("kt.c.dao.BoardDAO.selectByNo", no);
 			
 		} catch(Exception e) {
 			throw e;
 
 		} finally {
-			try {rs.close();} catch (Exception e) {}
-			try {pstmt.close();} catch (Exception e) {}
-			try {con.close();} catch (Exception e) {}
+			try {sqlSession.close();} catch (Exception e) {}
 		}
 	}
 
 	public void insert(BoardVO board) throws Exception {
-		StringBuilder sql = new StringBuilder();
-		sql.append("insert into t_board(no, title, writer, content )");
-		sql.append("  values(?, ?, ?, ?) ");
-
-		Connection con = null;
-		PreparedStatement pstmt = null;
-
+		SqlSession sqlSession = sqlSessionFactory.openSession();
+		
 		try {
-			con = dataSource.getConnection();
-			pstmt = con.prepareStatement(sql.toString());
-			int index = 1;
-			pstmt.setInt(index++, board.getNo());
-			pstmt.setString(index++, board.getTitle());
-			pstmt.setString(index++, board.getWriter());
-			pstmt.setString(index++, board.getContent());
-
-			pstmt.executeUpdate();
+			sqlSession.insert("kt.c.dao.BoardDAO.insert", board);
+			sqlSession.commit();
+			
 		} catch(Exception e) {
 			throw e;
 
 		} finally {
-			try {pstmt.close();} catch (Exception e) {}
-			try {con.close();} catch (Exception e) {}
+			try {sqlSession.close();} catch (Exception e) {}
 		}
 	}
 
 	public void updateViewCnt(int no) throws Exception {
-		StringBuilder sb = new StringBuilder();
-		sb.append("update t_board ");
-		sb.append("  set view_cnt = view_cnt + 1 ");
-		sb.append(" where no = ? ");
+		SqlSession sqlSession = sqlSessionFactory.openSession();
 		
-		Connection con = null;
-		PreparedStatement pstmt = null;
-
 		try {
-			con = dataSource.getConnection();
-			pstmt = con.prepareStatement(sb.toString());
-			pstmt.setInt(1, no);
-			pstmt.executeUpdate();
+			// 두 번째 파라미터 반드시 객체가 들어가야 한다.
+			// JDK5에 추가된 기능: 기본 타입에 대해 Auto Boxing 기능 제공.
+			// 기본형(byte,short,int,...) => 
+			//					wrapper 객체(java.lang.Byte, java.lang.Short, ...)로 변환됨
+			// int 값이 Integer 객체로 자동 포장되어 전달된다.
+			sqlSession.update("kt.c.dao.BoardDAO.updateViewCnt", no /*new Integer(no)*/);
+			
+			// insert, update, delete인 경우 정상 실행했을 때 서버에 실행 결과를 적용할 것을 요청해야 한다.
+			sqlSession.commit();
 			
 		} catch(Exception e) {
 			throw e;
 
 		} finally {
-			try {pstmt.close();} catch (Exception e) {}
-			try {con.close();} catch (Exception e) {}
+			try {sqlSession.close();} catch (Exception e) {}
 		}
 	}
 
 	public void update(BoardVO board) throws Exception {
-		String sql = "update t_board ";
-		sql += "  set title = ? ";
-		sql += "    , content = ? ";
-		sql += " where no = ? ";
-
-		Connection con = null;
-		PreparedStatement pstmt = null;
+		SqlSession sqlSession = sqlSessionFactory.openSession();
 
 		try {
-			con = dataSource.getConnection();
-			pstmt = con.prepareStatement(sql);
-			pstmt.setString(1, board.getTitle());
-			pstmt.setString(2, board.getContent());
-			pstmt.setInt(3, board.getNo());
-			pstmt.executeUpdate();
+			sqlSession.update("kt.c.dao.BoardDAO.update", board);
+			sqlSession.commit();
 			
 		} catch(Exception e) {
 			throw e;
 
 		} finally {
-			try {pstmt.close();} catch (Exception e) {}
-			try {con.close();} catch (Exception e) {}
+			try {sqlSession.close();} catch (Exception e) {}
 		}
 	}
 
 	public void delete(int no) throws Exception {
-		StringBuilder sql = new StringBuilder();
-		sql.append("delete t_board ");
-		sql.append(" where no = ? ");
-		
-		Connection con = null;
-		PreparedStatement pstmt = null;
+		SqlSession sqlSession = sqlSessionFactory.openSession();
 
 		try {
-			con = dataSource.getConnection();
-			pstmt = con.prepareStatement(sql.toString());
-			pstmt.setInt(1, no);
-			pstmt.executeUpdate();
+			sqlSession.delete("kt.c.dao.BoardDAO.delete", no);
+			sqlSession.commit();
 			
 		} catch(Exception e) {
 			throw e;
 
 		} finally {
-			try {pstmt.close();} catch (Exception e) {}
-			try {con.close();} catch (Exception e) {}
+			try {sqlSession.close();} catch (Exception e) {}
 		}
 	}
 
 	public void insertFile(BoardFileVO fileVO) throws Exception {
-		StringBuilder sql = new StringBuilder();
-		sql.append("insert into t_board_file( ");
-		sql.append("  no, board_no, file_ori_name, file_save_name, file_size) ");
-		sql.append("  values(seq_t_board_file_no.nextval, ?, ?, ?, ?) ");
-
-		Connection con = null;
-		PreparedStatement pstmt = null;
-
+		SqlSession sqlSession = sqlSessionFactory.openSession();
+		
 		try {
-			con = dataSource.getConnection();
-			pstmt = con.prepareStatement(sql.toString());
-			int index = 1;
-			pstmt.setInt(index++, fileVO.getBoardNo());
-			pstmt.setString(index++, fileVO.getFileOriName());
-			pstmt.setString(index++, fileVO.getFileSaveName());
-			pstmt.setInt(index++, fileVO.getFileSize());
-
-			pstmt.executeUpdate();
+			sqlSession.insert("kt.c.dao.BoardDAO.insertFile", fileVO);
+			sqlSession.commit();
 			
 		} catch(Exception e) {
 			throw e;
 
 		} finally {
-			try {pstmt.close();} catch (Exception e) {}
-			try {con.close();} catch (Exception e) {}
-		}
+			try {sqlSession.close();} catch (Exception e) {}
+		}		
 	}
 
 	public List<BoardFileVO> selectFileBoard(int boardNo) throws Exception {
-
-		List<BoardFileVO> list = new ArrayList<>();
-
-		Connection con = null;
-		PreparedStatement pstmt = null;
-		ResultSet rs = null;
+		SqlSession sqlSession = sqlSessionFactory.openSession();
 
 		try {
-			con = dataSource.getConnection();
-			StringBuilder sb = new StringBuilder();
-			sb.append("select no, file_ori_name, file_save_name, ");
-			sb.append("       file_size ");
-			sb.append("  from t_board_file ");
-			sb.append(" where board_no = ? ");
-
-			pstmt = con.prepareStatement(sb.toString());
-			pstmt.setInt(1, boardNo);
-
-			rs = pstmt.executeQuery();
-
-			while(rs.next()) {
-				int no = rs.getInt("no");
-				String fileOriName = rs.getString("file_ori_name");
-				String fileSaveName = rs.getString("file_save_name");
-				int fileSize = rs.getInt("file_size");
-
-				BoardFileVO fileVO = new BoardFileVO();
-				fileVO.setNo(no);
-				fileVO.setFileOriName(fileOriName);
-				fileVO.setFileSaveName(fileSaveName);
-				fileVO.setFileSize(fileSize);
-
-				list.add(fileVO);
-
-			}
-			return list;
+			return sqlSession.selectList("kt.c.dao.BoardDAO.selectFileBoard", boardNo);
 			
 		} catch(Exception e) {
 			throw e;
 
 		} finally {
-			try {rs.close();} catch (Exception e) {}
-			try {pstmt.close();} catch (Exception e) {}
-			try {con.close();} catch (Exception e) {}
+			try {sqlSession.close();} catch (Exception e) {}
 		}
 	}
 
 	public void deleteFile(int boardNo) throws Exception {
-		StringBuilder sql = new StringBuilder();
-		sql.append("delete t_board_file ");
-		sql.append(" where board_no = ? ");
-		
-		Connection con = null;
-		PreparedStatement pstmt = null;
+		SqlSession sqlSession = sqlSessionFactory.openSession();
 
 		try {
-			con = dataSource.getConnection();
-			pstmt = con.prepareStatement(sql.toString());
-			pstmt.setInt(1, boardNo);
-			pstmt.executeUpdate();
+			sqlSession.delete("kt.c.dao.BoardDAO.deleteFile", boardNo);
+			sqlSession.commit();
+			
 		} catch(Exception e) {
 			throw e;
 
 		} finally {
-			try {pstmt.close();} catch (Exception e) {}
-			try {con.close();} catch (Exception e) {}
+			try {sqlSession.close();} catch (Exception e) {}
 		}
 	}
 }
